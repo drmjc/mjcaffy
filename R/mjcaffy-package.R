2e3457d5bc1147062983ef0c312993f36d72a909
Mark Cowley <m.cowley0@gmail.com>
2012-07-17 04:45:27 +0000

    added static imports

---
+++ R/mjcaffy-package.R
@@ -0,0 +1,19 @@
+#' Helper functions for dealing with Affymetrix data. 
+#' 
+#' \tabular{ll}{ 
+#' Package: \tab mjcaffy\cr
+#' Type: \tab Package\cr
+#' Version: \tab 1.0.0\cr
+#' Date: \tab 2012-07-17\cr
+#' License: \tab GPL-2\cr
+#' }
+#' 
+#' @name mjcaffy-package
+#' @aliases mjcaffy-package mjcaffy
+#' @docType package
+#' @author Mark Cowley
+#' 
+#' Maintainer: Mark Cowley <m.cowley@@garvan.org.au>
+#' @keywords package
+#' @import methods mjcbase
+NULL
