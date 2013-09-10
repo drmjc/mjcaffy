a7381012a469ba4ccc5491ab8355eacae9e3f044
Mark Cowley <m.cowley@garvan.org.au>
2013-09-11 00:10:49 +1000

    dropped file argument
    version bump to 1.0.2

--- R/affy.pivot.table.R
+++ R/affy.pivot.table.R
@@ -2,14 +2,12 @@
 #' 
 #' @param exp a \code{matrix}-like object of expression levels
 #' @param calls a \code{matrix}-like object of detection calls (P/M/A)
-#' @param file the output filename
 #' @return none.
 #' @author Mark Cowley
 #' @export
-#' @importFrom Biobase exprs
-affy.pivot.table <- function(exp, calls, file=NULL) {
-	if( !is.matrix.like(exp) ) exp <- exprs(exp)
-	if( !is.matrix.like(calls) ) calls <- exprs(calls)
+affy.pivot.table <- function(exp, calls) {
+	# if( !is.matrix.like(exp) ) exp <- exprs(exp)
+	# if( !is.matrix.like(calls) ) calls <- exprs(calls)
 	exp <- round(exp,4)
 	
     res <- interleave.columns(exp, calls)
@@ -17,9 +15,9 @@ affy.pivot.table <- function(exp, calls, file=NULL) {
     tmp2 <- rep(c("_Signal", "_Detection"), ncol(exp))
     colnames(res) <- paste(colnames(res), tmp2, sep="")
 
-    if( !is.null(file) ) {
-		write.delim(res, file, row.names=TRUE)
-	}
-
     invisible(res)
 }
+# CHANGELOG:
+# 2013-01-16: 
+# - dropped calls to exprs
+# - dropped file argument
